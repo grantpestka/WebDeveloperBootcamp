d8ec2538adb7ef02d67b41618538a10f51ad2ad6
Grant Pestka <grant.pestka@gmail.com>
2019-03-13 23:31:34 -0600

    JS Stalker Exercise

---
+++ Projects/012_JS_Stalker/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("first name");
+var lastName = prompt("last name");
+var age = prompt("age");
+
+console.log("Your name is "+firstName+" "+lastName);
+console.log("Your age is "+age);
