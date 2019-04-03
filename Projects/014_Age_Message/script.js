90bea749884809e15f1aa350cd5827423a23c014
Grant Pestka <grant.pestka@gmail.com>
2019-04-02 23:05:39 -0600

    Age Message Exercise take 8

--- Projects/014_Age_Message/script.js
+++ Projects/014_Age_Message/script.js
@@ -1,48 +1,61 @@
-var age = prompt("age");
+// var age = prompt("age");
+var age;
+var pmpt = prompt("age");
 var message = "";
 console.log(age);
 console.log("end setup");
 
 
 //This method of testing for a valid age excedes what had been taught in the class so far.  However, experiance has taught me that allowing for bad inputs is a recipe for disaster.
-function isValidAge(age) {
-	if (age < 0 || !Number(age)){
-		age = prompt("Please enter a valid age.");
-		// console.log(age);
-		isValidAge(age);
-		// console.log(age);
-		// console.log("repeat")
-		// return "bad";
-		return age;
+function isValidAge() {
+	if (pmpt < 0 || !Number(pmpt)){
+		console.log(pmpt);
+		console.log(requestValidAge());
 	} else {
-		console.log(age);
-		console.log("end")
-		// return 1;
-		// return "good";
-		return age;
+		return pmpt;
 	}
 }
 
+function requestValidAge (){
+	var input = prompt("Please enter a valid age.");
+	if (input <0 || !Number(input)){
+		console.log(input);
+		requestValidAge();
+		console.log(input);
+
+	}else{
+		console.log(input);
+		return input;
+		// return 1;
+	}
+	console.log(input);
+	return input;
+}
+
+
+age = isValidAge();
+// age = requestValidAge();
+console.log(age)
 // console.log(isValidAge(age));
-age = isValidAge(age);
+// // age = isValidAge(age);
 
-console.log(age);
+// console.log(age);
 
-if (age == 21){
-	message = message + "happy 21st birthday!! ";
-}else{
-	console.log("(21)"+age);
-}
+// if (age == 21){
+// 	message = message + "happy 21st birthday!! ";
+// }else{
+// 	console.log("(21)"+age);
+// }
 
-console.log(age);
-
-
-if (age%2 == 1){
-	message = message + "your age is odd";
-}else{
-	console.log("(odd)"+age);
-}
+// console.log(age);
 
 
-console.log(message);
-alert(message);
+// if (age%2 == 1){
+// 	message = message + "your age is odd";
+// }else{
+// 	console.log("(odd)"+age);
+// }
+
+
+// console.log(message);
+// alert(message);
