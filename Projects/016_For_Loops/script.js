5448a903467fb743990bc55e1410e8c5fa47c10c
Grant Pestka <grant.pestka@gmail.com>
2019-04-03 00:32:37 -0600

    For Loops Exercise edits

---
+++ Projects/016_For_Loops/script.js
@@ -0,0 +1,43 @@
+// var num = -10;
+
+// while (num <19){
+// 	console.log(num);
+// 	num ++ ;
+// }
+
+for (var i = -10; i < 19; i++) {
+	console.log(i);
+}
+
+// var num = 10;
+console.log("2------------------")
+// while (num >= 10 && num <40){
+// 	console.log(num);
+// 	num +=2 ;
+// }
+
+for (var i = 10; i < 40; i+=2) {
+	console.log(i);
+}
+
+// var num = 10;
+console.log("3------------------")
+
+for (var i =301; i < 330; i+=2) {
+	console.log(i);
+}
+
+// var num = 5;
+console.log("4--------------");
+// while (num <=50){
+// 	if( num % 5 === 0 && num % 3 === 0){
+// 		console.log(num);
+// 	}
+// 	num ++ ;
+// }
+
+for (var i =5; i < 50; i++) {
+	if( i % 5 === 0 && i % 3 === 0){
+		console.log(i);
+	}
+}
