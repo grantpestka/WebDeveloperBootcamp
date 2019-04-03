ab7ed2ea146fa71776d2666e4201f2996b591ab9
Grant Pestka <grant.pestka@gmail.com>
2019-04-02 23:29:43 -0600

    Age Message Exercise final

--- Projects/014_Age_Message/script.js
+++ Projects/014_Age_Message/script.js
@@ -1,61 +1,55 @@
-// var age = prompt("age");
-var age;
-var pmpt = prompt("age");
+var age = prompt("age");
+// var age;
+// var pmpt = prompt("age");
 var message = "";
-console.log(age);
-console.log("end setup");
+// console.log(age);
+// console.log("end setup");
 
 
-//This method of testing for a valid age excedes what had been taught in the class so far.  However, experiance has taught me that allowing for bad inputs is a recipe for disaster.
-function isValidAge() {
-	if (pmpt < 0 || !Number(pmpt)){
-		console.log(pmpt);
-		console.log(requestValidAge());
-	} else {
-		return pmpt;
-	}
-}
-
-function requestValidAge (){
-	var input = prompt("Please enter a valid age.");
-	if (input <0 || !Number(input)){
-		console.log(input);
-		requestValidAge();
-		console.log(input);
-
-	}else{
-		console.log(input);
-		return input;
-		// return 1;
-	}
-	console.log(input);
-	return input;
-}
-
-
-age = isValidAge();
+//After several hours of trying to figure out a recursive function with if statements that would repeatedly ask the user for the correct type of age input intil it was given, I have decided to wait for the "while" loops lesson and hope that they can solve this kind of problem.
+// function isValidAge() {
+// 	if (pmpt < 0 || !Number(pmpt)){
+// 		console.log(pmpt);
+// 		console.log(requestValidAge());
+// 	} else {
+// 		return pmpt;
+// 	}
+// }
+// function requestValidAge (){
+// 	var input = prompt("Please enter a valid age.");
+// 	if (input <0 || !Number(input)){
+// 		console.log(input);
+// 		requestValidAge();
+// 		console.log(input);
+// 	}else{
+// 		console.log(input);
+// 		return input;
+// 		// return 1;
+// 	}
+// 	console.log(input);
+// 	return input;
+// }
 // age = requestValidAge();
-console.log(age)
 // console.log(isValidAge(age));
-// // age = isValidAge(age);
+// age = isValidAge(age);
 
-// console.log(age);
+if (age <0 || !Number(age)){
+	age = prompt("Please enter a valid age.");
+}
 
-// if (age == 21){
-// 	message = message + "happy 21st birthday!! ";
-// }else{
-// 	console.log("(21)"+age);
-// }
+if (age == 21){
+	message = message + "happy 21st birthday!! ";
+}
 
-// console.log(age);
+if (age%2 == 1){
+	message = message + "your age is odd ";
+}
 
+if (Math.sqrt(age)%1 == 0){
+	message = message + "perfect square";
+}
 
-// if (age%2 == 1){
-// 	message = message + "your age is odd";
-// }else{
-// 	console.log("(odd)"+age);
-// }
+console.log(Math.sqrt(age)%1);
 
-
-// console.log(message);
-// alert(message);
+console.log(message);
+alert(message);
