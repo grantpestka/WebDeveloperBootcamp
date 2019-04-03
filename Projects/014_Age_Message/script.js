6b4c86bb8db256c1c47b205fd890de7bd27cbe5e
Grant Pestka <grant.pestka@gmail.com>
2019-04-02 21:15:09 -0600

    Age Message Exercise take 2

--- Projects/014_Age_Message/script.js
+++ Projects/014_Age_Message/script.js
@@ -1,30 +1,27 @@
-var input = prompt("age");
-var age;
+var age = prompt("age");
 var message = "";
-console.log(input);
 console.log(age);
 console.log("end setup");
 
 
 //This method of testing for a valid age excedes what had been taught in the class so far.  However, experiance has taught me that allowing for bad inputs is a recipe for disaster.
-function isValidAge(input) {
-	if (input < 0 || !Number(input)){
-		input = prompt("Please enter a valid age.");
-		isValidAge(input);
-		console.log(input);
+function isValidAge(age) {
+	if (age < 0 || !Number(age)){
+		age = prompt("Please enter a valid age.");
+		isValidAge(age);
 		console.log(age);
 		console.log("end function")
+		return true;
 
 	} else {
-		age = input;
+		return true;
 	}
 }
-// console.log(isValidAge(input));
 
 console.log(age);
 
 if (
-	isValidAge(input) && 
+	isValidAge(age) && 
 	age == 21){
 	message = message + "happy 21st birthday!! ";
 }
@@ -33,7 +30,7 @@ console.log(age);
 
 
 if (
-	isValidAge(input) && 
+	isValidAge(age) && 
 	age%2 == 1){
 	message = message + "your age is odd";
 }
