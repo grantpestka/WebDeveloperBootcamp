69224d0b46d7b9c28f11abe3c8dbbdd4b951a5e1
Grant Pestka <grant@thetissilentdata.com>
2019-04-09 21:34:29 -0600

    ColorChanger Exercise

--- Projects/020_ColorChanger/script.js
+++ Projects/020_ColorChanger/script.js
@@ -1,4 +1,11 @@
+//Change the color of the background of the body between 2 colors when the button is clicked
 var button = document.querySelector("button");
 button.addEventListener("click",function() {
-	document.querySelector("body").style.background = "green"
+	var bodyColor = document.querySelector("body").style.background;
+	if (bodyColor !== "green"){
+		document.querySelector("body").style.background = "green";
+	}else{
+		document.querySelector("body").style.background = "blue";
+	}
+	 
 })
