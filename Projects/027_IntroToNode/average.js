5c6ea7560055cc7147deade2717fe52787d814f3
Grant Pestka <grant.pestka@gmail.com>
2019-04-22 22:30:35 -0600

    Into to NPM

--- Projects/027_IntroToNode/average.js
+++ Projects/027_IntroToNode/average.js
@@ -5,12 +5,12 @@ function average(arr){
     }
     var avg = Math.round(sum/arr.length);
     // console.log("sum = " + sum);
-    // console.log("avg = " + avg);
+    console.log("avg = " + avg);
     // console.log("avg = " + arr.length);
     return avg;
 }
 
-var scores = [90,98,89,100,86,94];
+var scores = [90,98,89,100, 100,86,94];
 average(scores);
 
 var scores2 = [40,65,77,82,80,54,73,63,95,49];
