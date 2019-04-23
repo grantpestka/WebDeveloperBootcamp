82f10b8ddfa7da04d7ff19a73e444dd77396f34b
Grant Pestka <grant.pestka@gmail.com>
2019-04-22 22:04:12 -0600

    average exercise

---
+++ Projects/027_IntroToNode/average.js
@@ -0,0 +1,18 @@
+function average(arr){
+    var sum = 0;
+    for(var i = 0; i < arr.length; i++){
+        sum += arr[i]; 
+    }
+    var avg = Math.round(sum/arr.length);
+    // console.log("sum = " + sum);
+    // console.log("avg = " + avg);
+    // console.log("avg = " + arr.length);
+    return avg;
+}
+
+var scores = [90,98,89,100,86,94];
+average(scores);
+
+var scores2 = [40,65,77,82,80,54,73,63,95,49];
+
+average(scores2);
