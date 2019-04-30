c8243f0249b367c6dc2740f92a0087262f0f7e89
Grant Pestka <grant.pestka@gmail.com>
2019-04-29 21:34:47 -0600

    RESTful Blog EDIT and UPDATE Exercise

--- Projects/032_RESTfulBlog/app.js
+++ Projects/032_RESTfulBlog/app.js
@@ -1,4 +1,5 @@
 const bodyParser = require("body-parser"),
+methodOverride   = require("method-override"),
 mongoose         = require("mongoose"),
 express          = require("express"),
 app              = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost:27017/restful_blog", { useNewUrlParser: tr
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -64,6 +66,30 @@ app.get("/blogs/:id", (req, res)=>{
         }
     });
 });
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res)=>{
+    Blog.findById(req.params.id, (err, foundBlog)=>{
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", (req, res)=>{
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
+
+
 
 app.listen(3000,()=>{
     console.log("Server Running");
